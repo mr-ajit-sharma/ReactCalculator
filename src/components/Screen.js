9fb0925c146167f185a83ae4c163f6b099f741fb
mrkrezza1 <sajit1020@gmail.com>
2024-02-27 03:09:25 +0530

    here we have solve the issue of the deps

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import { CalcContext } from '../context/calcContext'
-import TextFit from 'react-textfit'
+// import TextFit from 'react-textfit'
 const Screen = () => {
     const {calc}=useContext(CalcContext)
   return (
-    <TextFit className='screen' mode="single" max={70}>{calc.num?calc.num:calc.res}</TextFit>
+    // <TextFit className='screen' mode="single" max={70}>{calc.num?calc.num:calc.res}</TextFit>
+    <h3 className='screen'>{calc.num?calc.num:calc.res}</h3>
   )
 }
 
